d46096d1099998062bb33b25901f1a8caa440ee7
adiri77 <106059230+adiri77@users.noreply.github.com>
2024-04-22 06:38:48 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
 const User = require('../models/User');
-const Student=require('../models/schema')
+const Student=require('../models/schema');
 
 exports.register = async (req, res) => {
   try {
